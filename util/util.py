0e9aa6f804125f5950bbe4a90c489514f5e139d6
Michael V <michaelverdegaal@hotmail.nl>
2018-11-10 19:53:19 +0100

    Fix IOOB error

--- util/util.py
+++ util/util.py
@@ -14,11 +14,11 @@ def url_remove(reaction_list, keep_url=True):
     """
     if keep_url:
         for idx, item in enumerate(reaction_list):
-            if not validators.url(item[1]):
+            if not validators.url(item):
                 reaction_list.pop(idx)
     else:
         for idx, item in enumerate(reaction_list):
-            if validators.url(item[1]):
+            if validators.url(item):
                 reaction_list.pop(idx)
     return reaction_list
 
