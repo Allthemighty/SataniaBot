034efa6004a8977fb5f0cba3fddc51a3f324b7d4
Michael V <michael.verdegaal@hva.nl>
2019-03-29 14:28:42 +0100

    post user rows less leisurely

--- src/modules/user/user_model.py
+++ src/modules/user/user_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, Integer, String
+from sqlalchemy import Column, BigInteger, Integer, String, ForeignKey
 
 from src import constants as const
 
@@ -9,7 +9,10 @@ class User(const.BASE):
     uid = Column(BigInteger, primary_key=True)
     username = Column(String)
     reaction_count = Column(Integer)
+    from_server = Column(BigInteger, ForeignKey('server.server_id'), nullable=False)
 
-    def __str__(self):
-        return "ID: {}| Username {}| Reactions triggered: {}" \
-            .format(self.uid, self.username, self.reaction_count)
+    def __repr__(self):
+        return (f"ID: [{self.uid}], "
+                f"Username [{self.username}], "
+                f"Reaction count: [{self.reaction_count}], "
+                f"From_server: [{self.from_server}]")
